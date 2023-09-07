b8ef7e7de4525da070d553de40b0cbbac1fc60a9
Xinshi123 <a13418404439@gmail.com>
2023-09-07 22:29:41 +0800

    version 1.0.0

--- src/App.js
+++ src/App.js
@@ -4,24 +4,20 @@ import {
 } from 'react'
 import axios from 'axios'
 import './index.css'
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, Select, Card, Col, Row, Statistic, notification, Space, Typography, Divider } from 'antd';
 
 const App = () => {
-  // const [formData, setData] = useState({
-  //   key1: ''
-  // })
-  const [getResponseData, setGetResponseData] = useState();
 
-  // const handleChange = (event) => {
-  //     const { name, value } = event.target
-  //     setData({
-  //       ...formData,
-  //       [name]: nameValue,
-  //     })
-  //   }
+  const [getResponseData, setGetResponseData] = useState();
+  const [toShow, SetToShow] = useState(false)
+  const [state, setState] = useState({
+    province: '',
+    radio: ''
+  });
+  const { Text, Link, Title } = Typography;
+
 
   const handleSubmit = async (event) => {
-    // event.preventDefault();
 
     try {
       const response = await axios.post('http://127.0.0.1:5000/api/endpoint',
@@ -39,54 +35,120 @@ const App = () => {
     }
   }
 
-  const province = getResponseData
-    ? getResponseData.area
-    : ''
-  const radio = getResponseData
-    ? getResponseData.content[0].value
-    : ''
+  const select_city = getResponseData
+    ? getResponseData.content.map(item => ({
+      value: item.name,
+      label: item.name
+    }))
+    : [{ value: 'disabled', label: 'Disabled', disabled: true }]
 
   const [form] = Form.useForm();
   const nameValue = Form.useWatch('province', form);
-  // console.log(nameValue)
+
+  const onChange = (value) => {
+    console.log(value)
+    const getObjectByName = (name) => {
+      return getResponseData.content.find(item => item.name === name);
+    };
+    const obj = getObjectByName(value.name)
+    console.log(obj)
+    const province = value
+      ? value.name
+      : ''
+    const radio = value
+      ? obj.value
+      : ''
+    setState({
+      province,
+      radio
+    });
+    SetToShow(true)
+  };
+  const onFinishFailed = () => {
+    openNotification()
+  }
+
+  const [api, contextHolder] = notification.useNotification();
+  const openNotification = () => {
+    api.open({
+      message: 'Finish Failed',
+      description:
+        'Please fill with the form .',
+      duration: 3,
+    });
+  };
 
   return (
     <div>
-      {/* <div class="card">
-        <form
-          class="card__form"
-          onSubmit={handleSubmit}>
-          <input name="key1"
-            value={formData.key1}
-            onChange={handleChange}
-            type="text"
-            placeholder="province" />
-          <input
-            type='text'
-          />
-          <br />
-          <Button type="submit">Primary Button</Button>
-          <button class="sign-up" type="submit">Sub<span style={{ "color": "yellow" }}>mit</span></button>
-        </form>
-      </div> */}
-      {
-        getResponseData
-          ? <p>
-            {province}地区的辐射值为：{radio}
-          </p>
-          : ''
-      }
-      <Form form={form} layout="vertical" autoComplete="off" onFinish={handleSubmit}>
-        <Form.Item name="province" label="province">
-          <Input />
-        </Form.Item>
-        {/* <Form.Item name="age" label="Age (Not Watch)">
-          <InputNumber />
-        </Form.Item> */}
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form>
+      <Row gutter={1} type="flex" justify='center' align="middle"
+        style={{ minHeight: '73vh' }}>
+        <Col span={4}></Col>
+        <Col span={10} className='NeoCard'>
+          <Card style={{ display: 'flex', justifyContent: 'center' }}>
+            <Title type="flex" justify='center' align="middle">RayDar</Title>
+            <Form form={form} layout="vertical"
+              style={{
+                maxWidth: 150,
+              }} autoComplete="off"
+              onFinish={onChange}
+              onFinishFailed={onFinishFailed}>
+              <Form.Item
+                name="province"
+                label="Province"
+                rules={[{ required: true, message: 'Please input province name!' }]}>
+                <Input onBlur={handleSubmit} />
+              </Form.Item>
+              {contextHolder}
+
+              <Form.Item name="name" label="Zone">
+                <Select
+                  // defaultValue="lucy"
+                  showSearch
+                  placeholder="Select a District"
+                  optionFilterProp="children"
+                  // onChange={onChangeCity}
+                  options={select_city}
+                />
+              </Form.Item>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Button type="primary" htmlType="submit" value="large">
+                  Submit
+                </Button>
+              </div>
+            </Form>
+          </Card>
+          {
+            toShow
+              ? <Card bordered={true}>
+                <Statistic
+                  title={state.province}
+                  value={state.radio}
+                  precision={2}
+                  valueStyle={{
+                    color: 'black',
+                  }}
+                // suffix="%"
+                />
+              </Card>
+              : <div></div>
+          }
+
+        </Col>
+        <Col span={4}></Col>
+      </Row>
+      <Row gutter={1} type="flex" justify='center' align="middle"
+      >
+        <Space>
+          <Typography>
+            <Divider>Made by</Divider>
+            <Text type="secondary">Xinshui</Text>
+            <Text type="secondary">&nbsp;|&nbsp;Inspired by </Text>
+            <Link href="https://sspai.com/post/82619" target="_blank">
+              Fairyex
+            </Link>
+          </Typography>
+        </Space>
+      </Row>
     </div >
   )
 }
